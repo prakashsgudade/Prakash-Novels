f8768a730582a5b1e1bbf51242cc3d4cec52bae8
prakashsgudade <prakashsgudade@gmail.com>
2026-01-24 08:02:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function scrollToNovels() {
 function toggleDark() {
   document.body.classList.toggle("dark");
 }
+/* ================= FONT SIZE CONTROLS ================= */
+.font-controls {
+  position: fixed;
+  top: 15px;
+  left: 15px;
+  display: flex;
+  gap: 6px;
+  z-index: 999;
+}
+
+.font-controls button {
+  padding: 6px 10px;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  font-size: 14px;
+  background: #000;
+  color: #fff;
+}
+
+body.dark .font-controls button {
+  background: #fff;
+  color: #000;
+    }
