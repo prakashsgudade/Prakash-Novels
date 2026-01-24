b80e7ad7cc97aa98779c47330b5c551142461811
prakashsgudade <prakashsgudade@gmail.com>
2026-01-24 11:36:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ function scrollToNovels() {
 function toggleDark() {
   document.body.classList.toggle("dark");
 }
+// Dark / Light Toggle
+function toggleMode() {
+  document.body.classList.toggle("light");
+}
+
+// Auto Progress (demo)
+let progress = 0;
+setInterval(() => {
+  if (progress < 15) {
+    progress++;
+    let percent = Math.floor((progress / 15) * 100);
+    document.querySelector(".progress-bar").style.width = percent + "%";
+    document.querySelector(".progress-text").innerText =
+      "Reading Progress: " + percent + "%";
+  }
+}, 300);
