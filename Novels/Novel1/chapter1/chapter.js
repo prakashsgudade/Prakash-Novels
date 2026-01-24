619dde519601e4fd33f8046aa01c1a01d2529672
prakashsgudade <prakashsgudade@gmail.com>
2026-01-24 18:50:50 +0530

    Update chapter.js

--- Novels/Novel1/chapter1/chapter.js
+++ Novels/Novel1/chapter1/chapter.js
@@ -1,1 +1,14 @@
+const pages = document.querySelectorAll(".page");
+const counter = document.getElementById("pageCounter");
 
+window.addEventListener("scroll", () => {
+  let current = 1;
+
+  pages.forEach((page, i) => {
+    if (page.getBoundingClientRect().top < window.innerHeight / 2) {
+      current = i + 1;
+    }
+  });
+
+  counter.innerText = "Page " + current;
+});
