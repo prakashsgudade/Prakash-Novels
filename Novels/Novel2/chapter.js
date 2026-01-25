f353e4dac8b1fd4a7bc393ec0ea7ba7905dcdbe7
prakashsgudade <prakashsgudade@gmail.com>
2026-01-25 11:41:52 +0530

    Update chapter.js

--- Novels/Novel2/chapter.js
+++ Novels/Novel2/chapter.js
@@ -1,3 +1,18 @@
+const pages = document.querySelectorAll(".page");
+const counter = document.getElementById("pageCounter");
+const totalPages = pages.length;
+
+window.addEventListener("scroll", () => {
+  let current = 1;
+  pages.forEach((page, i) => {
+    if (page.getBoundingClientRect().top < window.innerHeight / 2) {
+      current = i + 1;
+    }
+  });
+  counter.innerText = "Page " + current + " / " + totalPages;
+});
+
+/* DARK MODE */
 function toggleDark() {
   document.body.classList.toggle("dark");
-}
+    }
