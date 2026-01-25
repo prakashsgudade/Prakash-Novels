5fd0726e5cfc46c792fd830795b248e529701a37
prakashsgudade <prakashsgudade@gmail.com>
2026-01-25 10:21:10 +0530

    Update chapter.js

--- Novels/Novel2/chapter.js
+++ Novels/Novel2/chapter.js
@@ -2,25 +2,15 @@ function toggleDark() {
   document.body.classList.toggle("dark");
 }
 
-function goHome() {
-  window.location.href = "../../index.html";
-}
-
-function goNovel() {
-  window.location.href = "../index.html";
-}
-
-// PAGE COUNT ON SCROLL
 const pages = document.querySelectorAll(".page");
-const pageNum = document.getElementById("currentPage");
+const pageNo = document.getElementById("pageNo");
 
 window.addEventListener("scroll", () => {
-  let index = 0;
-  pages.forEach((page, i) => {
-    const rect = page.getBoundingClientRect();
-    if (rect.top < window.innerHeight / 2) {
-      index = i + 1;
+  let current = 1;
+  pages.forEach((p, i) => {
+    if (p.getBoundingClientRect().top < window.innerHeight / 2) {
+      current = i + 1;
     }
   });
-  pageNum.innerText = index || 1;
+  pageNo.innerText = current;
 });
