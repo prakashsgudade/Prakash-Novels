f2f53c9fb6768e54ebe6f92b5924c82c58830879
prakashsgudade <prakashsgudade@gmail.com>
2026-01-25 11:29:20 +0530

    Update chapter.js

--- Novels/Novel2/chapter.js
+++ Novels/Novel2/chapter.js
@@ -1,16 +1,3 @@
 function toggleDark() {
   document.body.classList.toggle("dark");
 }
-
-const pages = document.querySelectorAll(".page");
-const currentPage = document.getElementById("currentPage");
-
-window.addEventListener("scroll", () => {
-  let page = 1;
-  pages.forEach((p, i) => {
-    if (p.getBoundingClientRect().top < window.innerHeight / 2) {
-      page = i + 1;
-    }
-  });
-  currentPage.innerText = page;
-});
