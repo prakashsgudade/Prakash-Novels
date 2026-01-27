1cf64c48fb9cab5acad07a37f38444d62d91dde0
prakashsgudade <prakashsgudade@gmail.com>
2026-01-27 13:42:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-function scrollToNovels() {
-  document.getElementById("novels")
-    .scrollIntoView({ behavior: "smooth" });
-}
-
-function toggleDark() {
-  document.body.classList.toggle("dark");
-}
-// Dark / Light Toggle
-function toggleMode() {
-  document.body.classList.toggle("light");
-}
-
-// Auto Progress (demo)
-let progress = 0;
-setInterval(() => {
-  if (progress < 15) {
-    progress++;
-    let percent = Math.floor((progress / 15) * 100);
-    document.querySelector(".progress-bar").style.width = percent + "%";
-    document.querySelector(".progress-text").innerText =
-      "Reading Progress: " + percent + "%";
-  }
-}, 300);
+// Soft welcome effect
+document.addEventListener("DOMContentLoaded",()=>{
+  console.log("📖 Prakash Novel Page Loaded");
+});
